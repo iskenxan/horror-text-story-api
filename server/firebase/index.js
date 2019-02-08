e43f0edb22f8def6df9d734d6a070470771ae3bc
iskenxan <iskenxan11@gmail.com>
2019-02-08 08:46:31 -0600

    fixing build

--- server/firebase/index.js
+++ server/firebase/index.js
@@ -1,27 +1,13 @@
 import * as admin from 'firebase-admin';
 import serviceAccount from '../firebase-admin-sdk';
 
-const { PROJECT_ID, CLIENT_EMAIL, PRIVATE_KEY } = process.env;
-
 
 const initializeFirebaseConnection = () => {
-  if (PROJECT_ID && CLIENT_EMAIL && PRIVATE_KEY) {
-    admin.initializeApp({
-      credential: admin.credential.cert({
-        projectId: PROJECT_ID,
-        clientEmail: CLIENT_EMAIL,
-        privateKey: PRIVATE_KEY,
-      }),
-      databaseURL: 'https://travelguide-bf6df.firebaseio.com',
-      storageBucket: 'travelguide-bf6df.appspot.com',
-    });
-  } else {
-    admin.initializeApp({
-      credential: admin.credential.cert(serviceAccount),
-      databaseURL: 'https://travelguide-bf6df.firebaseio.com',
-      storageBucket: 'travelguide-bf6df.appspot.com',
-    });
-  }
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: 'https://travelguide-bf6df.firebaseio.com',
+    storageBucket: 'travelguide-bf6df.appspot.com',
+  });
 };
 
 initializeFirebaseConnection();
