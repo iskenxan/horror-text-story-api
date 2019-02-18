784f3a889f0373c08c1dbe78ea31412f82f04c62
iskenxan <iskenxan11@gmail.com>
2019-02-18 09:00:15 -0600

    change popular feed allowance

--- server/rest-handlers/feed/ranking-feed.js
+++ server/rest-handlers/feed/ranking-feed.js
@@ -30,7 +30,7 @@ const getPost = (posts, postId) => {
 const addPostToRankingFeed = (rankedFeedItem) => {
   return getFeed()
     .then((feedItems) => {
-      if (Object.keys(feedItems).length >= 3) return;
+      if (Object.keys(feedItems).length >= 50) return;
       feedItems[rankedFeedItem.id] = rankedFeedItem;
       return updateFeed(feedItems);
     });
