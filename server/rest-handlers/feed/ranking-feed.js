350e1d9226b63f825d09ec820fb4d23fa0f4a35c
iskenxan <iskenxan11@gmail.com>
2019-02-18 09:11:35 -0600

    adjust popular feed

--- server/rest-handlers/feed/ranking-feed.js
+++ server/rest-handlers/feed/ranking-feed.js
@@ -30,7 +30,7 @@ const getPost = (posts, postId) => {
 const addPostToRankingFeed = (rankedFeedItem) => {
   return getFeed()
     .then((feedItems) => {
-      if (Object.keys(feedItems).length >= 50) return;
+      if (Object.keys(feedItems).length >= 60) return;
       feedItems[rankedFeedItem.id] = rankedFeedItem;
       return updateFeed(feedItems);
     });
