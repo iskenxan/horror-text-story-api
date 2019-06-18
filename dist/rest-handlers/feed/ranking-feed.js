7cb97ff04fa90f645646d277373193cd1271eedb
iskenxan <iskenxan11@gmail.com>
2019-06-18 09:36:07 -0500

    rebuild

--- dist/rest-handlers/feed/ranking-feed.js
+++ dist/rest-handlers/feed/ranking-feed.js
@@ -14,8 +14,6 @@ var _user2 = _interopRequireDefault(_user);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
-
 var getFeed = function getFeed() {
   return _firebase.db.collection('ranking-feed').doc('feed').get().then(function (snapshot) {
     var _snapshot$data = snapshot.data(),
@@ -126,82 +124,44 @@ var addNewFavoriteToPostRank = function addNewFavoriteToPostRank(rankedFeedItem)
   return addReactionToPostRank(rankedFeedItem, 'favoriteCount');
 };
 
-var fetchAndUpdateTimestamp = function () {
-  var _ref = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee(feedItem) {
-    var author, id, publishedPost, lastUpdated;
-    return regeneratorRuntime.wrap(function _callee$(_context) {
-      while (1) {
-        switch (_context.prev = _context.next) {
-          case 0:
-            author = feedItem.author, id = feedItem.id;
-            _context.next = 3;
-            return _user2.default.getPublished(author, id);
+var fetchAndUpdateTimestamp = function fetchAndUpdateTimestamp(feedItem) {
+  var author = feedItem.author,
+      id = feedItem.id;
 
-          case 3:
-            publishedPost = _context.sent;
 
-            if (publishedPost) {
-              _context.next = 6;
-              break;
-            }
+  return _user2.default.getPublished(author, id).then(function (doc) {
+    if (!doc.exists) return;
 
-            return _context.abrupt('return');
+    var _doc$data = doc.data(),
+        lastUpdated = _doc$data.lastUpdated;
 
-          case 6:
-            lastUpdated = publishedPost.lastUpdated;
+    feedItem.lastUpdated = lastUpdated;
 
-            feedItem.lastUpdated = lastUpdated;
-
-          case 8:
-          case 'end':
-            return _context.stop();
-        }
-      }
-    }, _callee, undefined);
-  }));
-
-  return function fetchAndUpdateTimestamp(_x) {
-    return _ref.apply(this, arguments);
-  };
-}();
+    return feedItem;
+  });
+};
 
 var checkAndFixTimestamp = function checkAndFixTimestamp(feedItems) {
-  var fixItems = feedItems.filter(function (item) {
+  var fixItems = _lodash2.default.filter(feedItems, function (item) {
     return item.lastUpdated === 0;
   });
-  var promises = fixItems.forEach(function (fixItem) {
+  var promises = fixItems.map(function (fixItem) {
     return fetchAndUpdateTimestamp(fixItem);
   });
-
-  return Promise.all(promises);
+  if (fixItems.length <= 0) return Promise.resolve();
+  return Promise.all(promises).then(function () {
+    return updateFeed(feedItems);
+  });
 };
 
 var getRankedFeed = function getRankedFeed() {
-  return getFeed().then(function () {
-    var _ref2 = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee2(feedItems) {
-      return regeneratorRuntime.wrap(function _callee2$(_context2) {
-        while (1) {
-          switch (_context2.prev = _context2.next) {
-            case 0:
-              _context2.next = 2;
-              return checkAndFixTimestamp(feedItems);
-
-            case 2:
-              feedItems = _context2.sent;
-              return _context2.abrupt('return', feedItems);
-
-            case 4:
-            case 'end':
-              return _context2.stop();
-          }
-        }
-      }, _callee2, undefined);
-    }));
-
-    return function (_x2) {
-      return _ref2.apply(this, arguments);
-    };
-  }());
+  var feedItems = void 0;
+  return getFeed().then(function (items) {
+    feedItems = items;
+    return checkAndFixTimestamp(feedItems);
+  }).then(function () {
+    return orderRankedFeedItems(feedItems);
+  });
 };
 
 module.exports = {
